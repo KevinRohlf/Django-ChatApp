cb963a679ae21b38caef126993b444906afdd8d3
Kevin Rohlf <123676073+KevinRohlf@users.noreply.github.com>
2023-07-05 21:23:16 +0200

    added register & chat response via js

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -1,15 +1,20 @@
+from django.http import JsonResponse
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from .models import Message, Chat
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.core import serializers
 
 @login_required(login_url='/login/')
 def index(request):
     if request.method == 'POST':
         print('received data: ' + request.POST['textmessage'])
         myChat = Chat.objects.get(id=1)
-        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+        new_message = Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+        serialized_obj = serializers.serialize('json', [new_message,])
+        return JsonResponse(serialized_obj[1:-1], safe=False)
     chatMessages = Message.objects.filter(chat__id=1)
     return render(request, 'chat/index.html', {'messages': chatMessages})
 
@@ -19,7 +24,15 @@ def login_view(request):
         user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
         if user:
             login(request, user)
+            print(redirect)
             return HttpResponseRedirect(request.POST.get('redirect'))
         else:
             return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
     return render(request, 'auth/login.html', {'redirect': redirect})
+
+def register_view(request):
+    if request.method == 'POST':
+        user = User.objects.create_user(username=request.POST.get('username'), email=request.POST.get('email'), password=request.POST.get('password'))
+        user.save()
+        return HttpResponseRedirect('/login/')
+    return render(request, 'auth/register.html')
